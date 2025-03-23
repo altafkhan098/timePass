aa085d55873e9cb32f8ef332a335222d19ea86ef
Jawed Alam <jawedmtp19@gmail.com>
2025-03-23 11:58:11 +0530

    TimePass

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// // Function to show an alert when the "View More" button is clicked in the Courses section
-// function showMessage() {
-//     alert("You will be redirected to a page with more courses soon!");
-// }
+// Function to show an alert when the "View More" button is clicked in the Courses section
+function showMessage() {
+    alert("You will be redirected to a page with more courses soon!");
+}
 
-// // Function to toggle visibility of profile details
-// function toggleProfile() {
-//     const profileDetails = document.getElementById("profileDetails");
-//     if (profileDetails.classList.contains("hidden")) {
-//         profileDetails.classList.remove("hidden");
-//     } else {
-//         profileDetails.classList.add("hidden");
-//     }
-// }
+// Function to toggle visibility of profile details
+function toggleProfile() {
+    const profileDetails = document.getElementById("profileDetails");
+    if (profileDetails.classList.contains("hidden")) {
+        profileDetails.classList.remove("hidden");
+    } else {
+        profileDetails.classList.add("hidden");
+    }
+}
